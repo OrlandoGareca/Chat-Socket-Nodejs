e46d5d93b98e346c8d6cb1527f00344ec97eb6c7
OrlandoGareca <orlando.dilmar.gareca.pena@gmail.com>
2018-07-06 21:26:16 -0400

    segundo commit

--- client/main.js
+++ client/main.js
@@ -1,1 +1,18 @@
 var socket = io.connect('http://192.168.100.8:6677',{'forceNew':true});
+
+socket.on('messages', function(data){
+    console.log(data);
+    render(data);
+});
+function render(data){
+    var html = data.map(function(message, index){
+        return (`
+             <div class="message">
+                <strong>${message.nickname}</strong>
+                <p>${message.text}</p>
+            </div>    
+        `);    
+    }).join('');
+
+    document.getElementById('messages').innerHTML = html;
+}
--- server/index.js
+++ server/index.js
@@ -9,8 +9,16 @@ app.get('/hola-mundo', function(req, res){
     res.status(200).send('hola mundo desde una ruta')
 });
 
+var messages = [{
+    id: 1,
+    text: 'Bienvenido al chat privado de docket.io y NodeJS',
+    nickname: 'Bot -orlandogareca'
+}]
+
 io.on('connection' , function(socket){
+    
     console.log("El nodo con IP: "+socket.handshake.address+" se a conectado...");
+    socket.emit('messages', messages);
 });
 
 server.listen(6677, function(){
